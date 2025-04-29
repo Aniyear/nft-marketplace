981c8a056fffa3d83a46290a4ff60d8617ad8f2f
unknown <acer.aniyar.05@gmail.com>
2025-04-29 11:33:33 +0500

    fix: host environment

--- bloskfine.py
+++ bloskfine.py
@@ -101,4 +101,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) # Получаем порт из переменной окружения или используем 5000 по умолчанию
+    app.run(host='0.0.0.0', port=port)
